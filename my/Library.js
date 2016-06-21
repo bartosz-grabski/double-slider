8ad4fc05acdc36007ad281afece61ede4d71a6bc
Arkadiusz Daniluk <arkadiusz.daniluk@idium.no>
2016-06-21 15:12:10 +0200

    dynamic content pane

--- my/Library.js
+++ my/Library.js
@@ -1,55 +1,65 @@
 define([
     "dojo/_base/declare",
-    "dojo/parser",     
+    "dojo/parser",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!/my/LibraryTemplate.html",
     "dojo/dom-class",
-    "dojo/on",
-    "dojox/gesture/swipe",
-    "dojox/mobile",
-    "dojox/mobile/parser",
-    "dojox/mobile/Carousel",
+    "dijit/layout/ContentPane",
+    "dijit/registry",
     "dojo/domReady!",
-    ], function(declare, parser, _WidgetBase, _TemplatedMixin, template, domClass){
+], function(declare, parser, _WidgetBase, _TemplatedMixin, template, domClass, ContentPane, registry){
 
 
-        var menuCssSelector = 'swipe-container';
-        var contentCssSelector = 'library-content';
+    var menuCssSelector = 'swipe-container';
+    var contentCssSelector = 'library-content';
 
 
-        return declare([_WidgetBase, _TemplatedMixin], {
+    return declare([_WidgetBase, _TemplatedMixin], {
 
-            templateString: template,
+        templateString: template,
 
-            activateMenuItem : function(swiper) {
-                console.log(swiper.activeIndex % 3);
+        activateMenuItem : function(swiper) {
+            console.log(swiper.activeIndex % 3);
 
-                //swiper.
+            //swiper.
 
-            },
+        },
 
-            disableMenuItem : function(swiper) {
-                console.log(swiper);
-            },
+        disableMenuItem : function(swiper) {
+            console.log(swiper);
+        },
 
+        postCreate: function() {
 
-            postCreate: function() {
-
-                var menuSwiper = new Swiper ('.swiper-container', {
+            var menuSwiper = new Swiper ('.swiper-container', {
                 // Optional parameters
                 direction: 'horizontal',
                 loop: true,
-    
+
                 centeredSlides : true,
                 slidesPerView: 3,
                 spaceBetween: 30,
                 onSlideChangeEnd: this.menuItemChanged,
                 onSliderMove: this.disableActiveMenuItem
-})        
+            })
 
+        },
+
+        loadPane: function(evt){
+            var targetDomNode = evt.target;
+            console.log(targetDomNode);
+            if(registry.byId("mainContentPane") != undefined){
+                registry.byId("mainContentPane").set("content",targetDomNode.innerHTML);
             }
-        });
-        
-      //parser.parse();
-  });
+            else{
+                new ContentPane({
+                    content:"<p>Optionally set new content now</p>",
+                    style:"height:125px"
+                }, "mainContentPane").startup();
+            }
+        }
+    });
+
+    //parser.parse();
+});
