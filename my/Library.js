1ea8c3b5aa55d2c9c998fcd28544d896c6b048eb
Bartosz Grabski <b.grabski92@gmail.com>
2016-06-21 14:21:28 +0200

    current state

--- my/Library.js
+++ my/Library.js
@@ -4,13 +4,14 @@ define([
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!/my/LibraryTemplate.html",
+    "dojo/dom-class",
     "dojo/on",
     "dojox/gesture/swipe",
     "dojox/mobile",
     "dojox/mobile/parser",
     "dojox/mobile/Carousel",
     "dojo/domReady!",
-    ], function(declare, parser, _WidgetBase, _TemplatedMixin, template){
+    ], function(declare, parser, _WidgetBase, _TemplatedMixin, template, domClass){
 
 
         var menuCssSelector = 'swipe-container';
@@ -21,18 +22,30 @@ define([
 
             templateString: template,
 
+            activateMenuItem : function(swiper) {
+                console.log(swiper.activeIndex % 3);
+
+                //swiper.
+
+            },
+
+            disableMenuItem : function(swiper) {
+                console.log(swiper);
+            },
+
 
             postCreate: function() {
 
                 var menuSwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+                // Optional parameters
+                direction: 'horizontal',
+                loop: true,
     
-    slidesPerView: 3,
-    spaceBetween: 30,
-    
-    
+                centeredSlides : true,
+                slidesPerView: 3,
+                spaceBetween: 30,
+                onSlideChangeEnd: this.menuItemChanged,
+                onSliderMove: this.disableActiveMenuItem
 })        
 
             }
