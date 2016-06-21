5965da65263e5ebbb1c955aaee37b208dcdf1717
Bartosz Grabski <b.grabski92@gmail.com>
2016-06-21 17:12:17 +0200

    programmatic way of adding elements

--- my/Library.js
+++ my/Library.js
@@ -4,22 +4,40 @@ define([
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dojo/text!/my/LibraryTemplate.html",
+    "dojo/text!/my/LibraryItemTemplate.html",
     "dojo/dom-class",
+    "dojo/dom-construct",
     "dijit/layout/ContentPane",
     "dijit/registry",
     "dojo/_base/lang",
-    "dojo/domReady!",
-], function(declare, parser, _WidgetBase, _TemplatedMixin, template, domClass, ContentPane, registry, lang){
+    "dojo/on",
+    "dojo/request/xhr",
+    "dojo/domReady!"
+], function(declare, parser, _WidgetBase, _TemplatedMixin, template, itemTemplate, domClass, domConstruct, ContentPane, registry, lang, on, xhr){
 
 
-    var menuCssSelector = 'swipe-container';
-    var contentCssSelector = 'library-content';
+    var xhrData = [{"name":"Forms","icon":"https://cdn2.iconfinder.com/data/icons/picol-vector/32/document_text-64.png","path":"/strype-templates/global/Forms","strypes":[{"name":"Contact form","path":"/strype-templates/global/Forms/contact-form-1"},{"name":"Contact form 2","path":"/strype-templates/global/Forms/contact-form-2"},{"name":"Contact form 3","path":"/strype-templates/global/Forms/contact-form-3"}]},{"name":"Galleries","icon":"https://cdn4.iconfinder.com/data/icons/miu/24/editor-images-pictures-photos-collection-glyph-64.png","path":"/strype-templates/global/Galleries","strypes":[{"name":"Cool gallery","path":"/strype-templates/global/Galleries/gallery-1"},{"name":"Even cooler gallery","path":"/strype-templates/global/Galleries/gallery-2"},{"name":"Even cooler gallery","path":"/strype-templates/global/Galleries/gallery-3"}]}]
+
+    var MenuItem =  declare("MenuItem", [dijit._WidgetBase,dijit._TemplatedMixin], {
+
+        templateString : itemTemplate,
+        name : '',
+
+        constructor : function(name, onClick) {
+            this.name = name;
+        }
+
+    });
 
 
     return declare([_WidgetBase, _TemplatedMixin], {
 
         templateString: template,
         activeElement : '',
+
+        constructor: function() {
+            console.log(arguments);
+        },
 
         slideFinished : function(swiper) {
             swiper.slideTo(swiper.activeIndex);
@@ -47,7 +65,12 @@ define([
 
         postCreate: function() {
 
-            var menuSwiper = new Swiper('.swiper-container', {
+            for (var i = 0; i < xhrData.length; i++) {
+                var newMenuItem = new MenuItem(xhrData[i].name);
+                newMenuItem.placeAt(this.menuWrapperDom);
+            }
+
+            var menuSwiper = new Swiper('.menu-container', {
                 // Optional parameters
                 direction: 'horizontal',
                 loop: true,
@@ -58,11 +81,15 @@ define([
                 onSlideChangeEnd: lang.hitch(this, this.slideFinished),
                 onTransitionStart: lang.hitch(this, this.slideStarted)
             });
+
+            this.activeElement = menuSwiper.slides[menuSwiper.activeIndex];
+
+
         },
 
         loadPane: function(node){
+
             var targetDomNode = node;
-            console.log(targetDomNode);
             if(registry.byId("mainContentPane") != undefined){
                 registry.byId("mainContentPane").set("content",targetDomNode.innerHTML);
             }
@@ -74,6 +101,9 @@ define([
             }
         }
     });
+
+
+
 
     //parser.parse();
 });
