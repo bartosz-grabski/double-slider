c1d25b05c804f4f27d886b7e521b9d3a52b463ed
Arkadiusz Daniluk <arkadiusz.daniluk@idium.no>
2016-06-22 09:12:48 +0200

    content pane && programmatic way of adding elements - merge

--- my/Library.js
+++ my/Library.js
@@ -10,15 +10,16 @@ define([
     "dijit/layout/ContentPane",
     "dijit/registry",
     "dojo/_base/lang",
+    "dijit/_WidgetsInTemplateMixin",
     "dojo/on",
     "dojo/request/xhr",
-    "dojo/domReady!"
-], function(declare, parser, _WidgetBase, _TemplatedMixin, template, itemTemplate, domClass, domConstruct, ContentPane, registry, lang, on, xhr){
-
+    "dojo/domReady!",
+], function(declare, parser, _WidgetBase, _TemplatedMixin, template, itemTemplate, domClass, domConstruct,
+            ContentPane, registry, lang, _WidgetsInTemplateMixin, on, xhr){
 
     var xhrData = [{"name":"Forms","icon":"https://cdn2.iconfinder.com/data/icons/picol-vector/32/document_text-64.png","path":"/strype-templates/global/Forms","strypes":[{"name":"Contact form","path":"/strype-templates/global/Forms/contact-form-1"},{"name":"Contact form 2","path":"/strype-templates/global/Forms/contact-form-2"},{"name":"Contact form 3","path":"/strype-templates/global/Forms/contact-form-3"}]},{"name":"Galleries","icon":"https://cdn4.iconfinder.com/data/icons/miu/24/editor-images-pictures-photos-collection-glyph-64.png","path":"/strype-templates/global/Galleries","strypes":[{"name":"Cool gallery","path":"/strype-templates/global/Galleries/gallery-1"},{"name":"Even cooler gallery","path":"/strype-templates/global/Galleries/gallery-2"},{"name":"Even cooler gallery","path":"/strype-templates/global/Galleries/gallery-3"}]}]
 
-    var MenuItem =  declare("MenuItem", [dijit._WidgetBase,dijit._TemplatedMixin], {
+    var MenuItem =  declare("MenuItem", [_WidgetBase, _TemplatedMixin], {
 
         templateString : itemTemplate,
         name : '',
@@ -29,8 +30,7 @@ define([
 
     });
 
-
-    return declare([_WidgetBase, _TemplatedMixin], {
+    return declare("StrypeLibrary", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 
         templateString: template,
         activeElement : '',
@@ -64,13 +64,12 @@ define([
         },
 
         postCreate: function() {
-
             for (var i = 0; i < xhrData.length; i++) {
                 var newMenuItem = new MenuItem(xhrData[i].name);
                 newMenuItem.placeAt(this.menuWrapperDom);
             }
 
-            var menuSwiper = new Swiper('.menu-container', {
+            var menuSwiper = new Swiper('.swiper-container', {
                 // Optional parameters
                 direction: 'horizontal',
                 loop: true,
@@ -87,18 +86,26 @@ define([
 
         },
 
+
+        initializeSwiperForNode: function(node){
+            console.log("initializing swpier for ",node);
+            var menuSwiper = new Swiper(node, {
+                // Optional parameters
+                direction: 'horizontal',
+                loop: true,
+                freeMode: true,
+                centeredSlides: true,
+                slidesPerView: 3,
+                spaceBetween: 100,
+                onSlideChangeEnd: lang.hitch(this, this.slideFinished),
+                onTransitionStart: lang.hitch(this, this.slideStarted)
+            });
+        },
         loadPane: function(node){
 
             var targetDomNode = node;
-            if(registry.byId("mainContentPane") != undefined){
-                registry.byId("mainContentPane").set("content",targetDomNode.innerHTML);
-            }
-            else{
-                new ContentPane({
-                    content:"<p>Optionally set new content now</p>",
-                    style:"height:125px"
-                }, "mainContentPane").startup();
-            }
+            this.contentPaneWidget.set("innerHTML",'<div class="swiper-container">'+ targetDomNode.innerHTML+'</div>');
+            this.initializeSwiperForNode(this.contentPaneWidget.innerHTML);
         }
     });
 
